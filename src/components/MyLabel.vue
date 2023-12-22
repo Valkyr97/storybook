3799ebe47c55aea33bc446a04374d605215f8cba
Valkyr97 <develloperjam.97@gmail.com>
2023-12-22 10:58:10 -0500

    Add documented args

---
+++ src/components/MyLabel.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+withDefaults(defineProps<{
+  /**
+   * Label text that will be used
+   */
+  label: string
+}>(), {
+  label: 'Hello World',
+})
+</script>
+
+<template>
+  <h1>{{ label }}</h1>
+</template>
+
+<style scoped>
+
+</style>
