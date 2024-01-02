d927bff136cca21c9e72e507f8999904430e4da1
Valkyr97 <develloperjam.97@gmail.com>
2024-01-02 00:45:22 -0500

    feat: Add tailwind theme to formkit

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,10 +1,11 @@
 import type {Preview} from "@storybook/vue3";
 import {setup} from '@storybook/vue3'
 import {plugin, defaultConfig} from '@formkit/vue'
+import config from '../formkit.config'
 import '../src/tailwind.css'
 
 setup((app) => {
-    app.use(plugin, defaultConfig)
+    app.use(plugin, defaultConfig(config))
 })
 
 const preview: Preview = {
